40cda283f783f6a3a41bd87e65a0524eec1972b7
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2024-05-20 07:58:28 +0900

    commands.go 업데이트

--- commands.go
+++ commands.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Commands struct {
-	vpnExecution []string
+	vpnExecutor  []string
 	btnFinder    []string
 	vpnPwFinder  []string
 	otpFinder    []string
@@ -20,16 +20,16 @@ var btnFinderApple string
 var otpFinderApple string
 
 func InitCommands() *Commands {
-	var vpnExecution, btnFinder, vpnPwFinder, otpFinder []string
+	var vpnExecutor, btnFinder, vpnPwFinder, otpFinder []string
 
 	// MacOS
 	if runtime.GOOS == "darwin" {
-		vpnExecution = []string{"bash", "-c", "open -n /Applications/Ivanti\\ Secure\\ Access.app"}
+		vpnExecutor = []string{"bash", "-c", "open -n /Applications/Ivanti\\ Secure\\ Access.app"}
 		btnFinder = []string{"osascript", "-e", btnFinderApple}
 		vpnPwFinder = []string{"bash", "-c", "security find-generic-password -s 'auto.vpn' -w"}
 		otpFinder = []string{"osascript", "-e", otpFinderApple}
 	} else {
 		log.Fatal("Not supported OS:", runtime.GOOS)
 	}
-	return &Commands{vpnExecution: vpnExecution, btnFinder: btnFinder, vpnPwFinder: vpnPwFinder, otpFinder: otpFinder}
+	return &Commands{vpnExecutor: vpnExecutor, btnFinder: btnFinder, vpnPwFinder: vpnPwFinder, otpFinder: otpFinder}
 }
